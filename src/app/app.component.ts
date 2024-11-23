8b5e67d517ac556f8dbfda14d21c34475fdbc987
TeaTucar <tea.tucar@gmail.com>
2024-11-23 14:47:50 +0100

    prva verzija kartice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { EventCardComponent } from './event-card/event-card.component';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatIconModule, RouterOutlet, EventCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
